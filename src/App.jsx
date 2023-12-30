e070b9290d07669f88ff54cd7d24e46e7b0cd1ba
salmamokhtaar <salmamuqtaarsiman@gmail.com>
2023-12-31 00:02:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  {Routes , Route} from 'react-router-dom'
+import Assignmet from './pages/Assignment'
+function App() {
+  return (
+    <div>
+      <Assignmet />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Assignment.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Assignment() {
+  return (
+      <div className='bg-green-400 w-full h-screen'>
+        <h1 className='text-white text-2xl ml-[190px]'>good<span className='font-bold text-3xl'>brief.</span>io</h1>
+        <div className='bg-white h-[530px] w-[70%] m-6  ml-[190px]'>
+         <div className='ml-8 pt-10'>
+            <h4 className=''>DESIGN BRIEF</h4>
+            <h1 className='font-bold text-2xl pt-3'>Company Name:</h1>
+            <h3 className='text-xl'>Main Bookworm</h3>
+            <div >
+                <h1 className='text-2xl font-bold'>Company Description:</h1>
+                <p className='text-xl pr-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum ex nemo modi placeat voluptate consectetur blanditiis labore nihil quae sint aliquid cupiditate, explicabo mollitia omnis numquam pariatur provident culpa dicta, alias similique nulla consequuntur non veritatis! Similique tenetur quae sequi?</p>
+            </div>
+            <div className='pt-3'>
+                <h1 className='text-2xl font-bold'>Job Description</h1>
+                <p className='text-xl pr-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum ex nemo modi placeat voluptate consectetur blanditiis labore nihil quae sint aliquid cupiditate, explicabo mollitia omnis numquam pariatur provident culpa dicta, alias similique nulla consequuntur non veritatis! Similique tenetur quae sequi?</p>
+            </div>
+         </div>
+         <h1 className='pl-6  pt-3 text-2xl font-bold'>Deadline</h1>
+         <h1 className='pl-6'>2days</h1>
+        </div>
+      </div>
+  )
+}
+
+export default Assignment
